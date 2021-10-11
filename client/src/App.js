190ce6de6dc114f1658e8733df3bd4fef7ae99ad
Your Name <beniyaakov23@gmail.com>
2021-10-12 00:26:08 +0300

    beni\setup\part2

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import NavBar from "./components/features/NavBar/NavBar";
+import Footer from "./components/features/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <NavBar />
+        <Footer />
+      </Router>
     </div>
   );
 }
