4728ca12d423ea70a0e24d0f20a4f4be5dc953e0
Your Name <beniyaakov23@gmail.com>
2021-10-12 20:50:05 +0300

    logo

--- client/src/components/features/NavBar/NavBar.js
+++ client/src/components/features/NavBar/NavBar.js
@@ -16,7 +16,7 @@ const NavBar = () => {
 {!isLogin&& 
              <Link to='/Login'><Button type="primary" className="button" text="התחברות"></Button> </Link>
 }
-             <img src="logo share it.jpg" width="100" />
+             <img src="logo share it2.png" width="200" height="80px" />
              <Link to="../"><img src="https://www.ibh.co.il/wp-content/uploads/2018/06/%D7%98%D7%A7-%D7%A7%D7%A8%D7%99%D7%99%D7%A8%D7%94-2.png" width="100" /></Link>
  
         </div>
