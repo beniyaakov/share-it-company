77518d927b294fb2e4b94bf52ab23882e2313300
kineretasiyahan <kineret9688@gmail.com>
2021-10-12 11:57:28 +0300

    commit

---
+++ client/src/components/features/Cards/HomeCardData.js
@@ -0,0 +1,27 @@
+export const HomeCardData = [
+    {
+        title: "מדריכים",
+        icon: "fas fa-chalkboard-teacher",
+        description: "פירוט על כל השפות הנלמדות בקורס,פירוט על כל השפות הנלמדות בקורס"
+
+    },
+    {
+        title: "בוגרים מספרים",
+        icon: 'fab fa-angellist',
+        description: "פירוט על כל השפות הנלמדות בקורס,פירוט על כל השפות הנלמדות בקורס"
+
+    },
+    {
+        title: "קורסים",
+        icon: "fas fa-graduation-cap",
+        description: "פירוט על כל השפות הנלמדות בקורס,פירוט על כל השפות הנלמדות בקורס"
+
+    },
+    {
+        title: "תכני לימוד",
+        icon: "fas fa-book-open",
+        description: "פירוט על כל השפות הנלמדות בקורס,פירוט על כל השפות הנלמדות בקורס"
+
+    },   
+
+]
--- client/src/components/features/Footer/Footer.js
+++ client/src/components/features/Footer/Footer.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import './footer.css'
+import {FacebookOutlined,MailOutlined,InstagramOutlined } from '@ant-design/icons'
 
 const Footer = () => {
     return (
         <div>
-            
+            <FacebookOutlined style={{margin:"25px"}}/>
+            <MailOutlined style={{margin:"25px"}}/>
+            <InstagramOutlined style={{margin:"25px"}}/> 
         </div>
     )
 }
-
 export default Footer
