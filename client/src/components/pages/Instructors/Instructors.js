e70b68f993d568f2ef151f90f69c0c41a76cc5cf
kineretasiyahan <kineret9688@gmail.com>
2021-10-13 02:06:32 +0300

    add Instructors

--- client/src/components/pages/Instructors/Instructors.js
+++ client/src/components/pages/Instructors/Instructors.js
@@ -1,35 +1,35 @@
 import React, { useState } from "react";
-import { Modal} from "antd";
+import { Modal } from "antd";
 import Card from '../../features/Cards/Card';
 import Button from '../../features/Button/Button';
 import "./Instructors.css";
 
-const {Meta} = Card;
+const { Meta } = Card;
 const InstructorsArray = [
   {
     title: "לינקדאין",
     description:
       "מדריך זה ינחה אותכם בבניית פרופיל שמתאים לתחילת תהליך בפיתוח קריירה בעולם הייטק",
-    img: "https://1000logos.net/wp-content/uploads/2017/03/Linkedin-Logo.png",
+    img: "https://miro.medium.com/max/719/0*kHdpA0lO-qQVbaXq.png",
     url: "https://e7.pngegg.com/pngimages/887/616/png-clipart-linkedin-icon-linkedin-text-rectangle-thumbnail.png",
   },
   {
     title: "גיטהב",
     description:
       "מדריך שיכוון אתכם להשתמש נכון בגיטהב ובכל הפעולות שרצוי לדעת בעבודה עם גיטהב",
-    img: "https://www.drupal.org/files/project-images/GitHub-Mark.png",
+    img: "https://miro.medium.com/max/719/0*kHdpA0lO-qQVbaXq.png",
     url: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png",
   },
   {
     title: "קורות חיים",
     description: "מדריך לבניית קורות חיים מקצועיים שיתאימו לעולם ההייטק ",
-    img: "",
+    img: "https://miro.medium.com/max/719/0*kHdpA0lO-qQVbaXq.png",
     url: "https://thumbs.dreamstime.com/b/cv-resume-filled-outline-icon-line-vector-sign-linear-colorful-pictogram-isolated-white-symbol-logo-illustration-pixel-perfect-107605453.jpg",
   },
   {
     title: "ריאיון עבודה",
     description: "מדריך שיכין אתכם לריאיון עבודה בצורה הכי טובה שיש",
-    img: "",
+    img: "https://miro.medium.com/max/719/0*kHdpA0lO-qQVbaXq.png",
     url: "https://www.taasuka.gov.il/he/Applicants/JobSearchStages/pages/",
   },
 ];
@@ -71,15 +71,10 @@ function Instructors() {
         </div>
       </Modal>
       <div className="instructorsDiv">
-        {InstructorsArray.map((item)=>{
-            return(
-                <>
-              
-            
-              </>
+        {InstructorsArray.map((item) => {
+          return 
 
-            )
-
+          
         })}
       </div>
     </div>
