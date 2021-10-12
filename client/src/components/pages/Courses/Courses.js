eeecadef08b2d154ca2545049f7e5a9d678f7ed7
eliasadmaso1 <76824469+eliasadmaso1@users.noreply.github.com>
2021-10-12 22:23:17 +0300

    commit

--- client/src/components/pages/Courses/Courses.js
+++ client/src/components/pages/Courses/Courses.js
@@ -5,9 +5,9 @@ import './Courses.css';
 
 const courses = [
     {site:"Go Code",url:"https://gocode.co.il/subjects",description:"קורס פתוח מקוון מרובה משתתפים , הוא קורס אינטרנטי מקוון הפתוח לקהל הרחב. לרוב הגישה אליו פתוחה ללא עלות דרך האינטרנט והוא מאפשר השתתפות למספר בלתי מוגבל של משתמשיםֿ",img:"https://gocode.co.il/logo.png"},
-    {site:"Udemy",url:"https://www.udemy.com/?utm_source=adwords-brand&utm_medium=udemyads&utm_campaign=Brand-Udemy_la.EN_cc.ROW&utm_term=_._ag_80315195513_._ad_535757779892_._de_c_._dm__._pl__._ti_kwd-310556426868_._li_1007983_._pd__._&utm_term=_._pd__._kw_udemy_._&matchtype=e&gclid=Cj0KCQjw5JSLBhCxARIsAHgO2SfAHOWRJy1QHzjfARG0b_HWqx0QrszVaaTBRF57Gykoy_q1xtEdMjcaAo5xEALw_wcB",description:"קורס פתוח מקוון מרובה משתתפים , הוא קורס אינטרנטי מקוון הפתוח לקהל הרחב. לרוב הגישה אליו פתוחה ללא עלות דרך האינטרנט והוא מאפשר השתתפות למספר בלתי מוגבל של משתמשים",img:"https://s.udemycdn.com/meta/default-meta-image-v2.png"},
+    {site:"Udemy",url:"https://www.udemy.com/?utm_source=adwords-brand&utm_medium=udemyads&utm_campaign=Brand-Udemy_la.EN_cc.ROW&utm_term=_._ag_80315195513_._ad_535757779892_._de_c_._dm__._pl__._ti_kwd-310556426868_._li_1007983_._pd__._&utm_term=_._pd__._kw_udemy_._&matchtype=e&gclid=Cj0KCQjw5JSLBhCxARIsAHgO2SfAHOWRJy1QHzjfARG0b_HWqx0QrszVaaTBRF57Gykoy_q1xtEdMjcaAo5xEALw_wcB",description:"קורס פתוח מקוון מרובה משתתפים , הוא קורס אינטרנטי מקוון הפתוח לקהל הרחב. לרוב הגישה אליו פתוחה ללא עלות דרך האינטרנט והוא מאפשר השתתפות למספר בלתי מוגבל של משתמשים",img:"https://digitalsharecropper.com/wp-content/uploads/2016/03/udemy-logo-e1604793745846.png"},
     {site:"To Code",url:"https://www.tocode.co.il/",description:"קורס פתוח מקוון מרובה משתתפים , הוא קורס אינטרנטי מקוון הפתוח לקהל הרחב. לרוב הגישה אליו פתוחה ללא עלות דרך האינטרנט והוא מאפשר השתתפות למספר בלתי מוגבל של משתמשים",img:"https://previews.123rf.com/images/dstarky/dstarky1701/dstarky170101187/69424051-coding-icon-or-logo-in-modern-line-style-high-quality-black-outline-pictogram-for-web-site-design-an.jpg"},
-    {site:"Coderbyte",url:"https://coderbyte.com/organizations?utm_term=coderbyte&utm_campaign=2020+%7C+Main+%E2%80%93+Coderbyte&utm_source=adwords&utm_medium=ppc&hsa_acc=9961249630&hsa_net=adwords&hsa_grp=101711018136&hsa_ver=3&hsa_kw=coderbyte&hsa_tgt=kwd-369057944540&hsa_mt=e&hsa_ad=437770531622&hsa_src=g&hsa_cam=10097874637&gclid=Cj0KCQjw5JSLBhCxARIsAHgO2SdtZmp1Ib76rQAlnvZrBjmwXrtLNs10GS1262obKDYsWX36sfNpoJEaAjGhEALw_wcB",description:"קורס פתוח מקוון מרובה משתתפים , הוא קורס אינטרנטי מקוון הפתוח לקהל הרחב. לרוב הגישה אליו פתוחה ללא עלות דרך האינטרנט והוא מאפשר השתתפות למספר בלתי מוגבל של משתמשים",img:"https://coderbytestaticimages.s3.amazonaws.com/consumer-v2/nav/coderbyte_logo_digital_multi_light.png"},
+    {site:"Coderbyte",url:"https://coderbyte.com/organizations?utm_term=coderbyte&utm_campaign=2020+%7C+Main+%E2%80%93+Coderbyte&utm_source=adwords&utm_medium=ppc&hsa_acc=9961249630&hsa_net=adwords&hsa_grp=101711018136&hsa_ver=3&hsa_kw=coderbyte&hsa_tgt=kwd-369057944540&hsa_mt=e&hsa_ad=437770531622&hsa_src=g&hsa_cam=10097874637&gclid=Cj0KCQjw5JSLBhCxARIsAHgO2SdtZmp1Ib76rQAlnvZrBjmwXrtLNs10GS1262obKDYsWX36sfNpoJEaAjGhEALw_wcB",description:"קורס פתוח מקוון מרובה משתתפים , הוא קורס אינטרנטי מקוון הפתוח לקהל הרחב. לרוב הגישה אליו פתוחה ללא עלות דרך האינטרנט והוא מאפשר השתתפות למספר בלתי מוגבל של משתמשים",img:"https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_de0840ae3396b61382ab711b086b7ba2/coderbyte-for-employers.png"},
 ]
 
 function Courses() {
@@ -27,11 +27,14 @@ function Courses() {
   
     return (
  <div>
-        
-<div class="container">
-<Button type="primary" onClick={showModal}>
-        אוסף קורס
+     <div class="btn">
+     <Button type="primary" onClick={showModal} style={{}}>
+        הוסף קורס
       </Button>
+     </div>
+       
+<div class="container">
+
       <Modal title="באפשרותך לשתף מידע על קורסים" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} okText="הוסף" cancelText="צא">
         <div className="modal">
         <input placeholder="שם הקורס"/>
--- client/src/components/pages/Instructors/Instructors.js
+++ client/src/components/pages/Instructors/Instructors.js
@@ -1,50 +1,92 @@
-import React, { useState } from 'react';
-import { Modal, Button } from 'antd';
-import './Instructors.css';
-
-
+import React, { useState } from "react";
+import { Modal, Button } from "antd";
+import "./Instructors.css";
 
 const InstructorsArray = [
-    {title:"dsfd",subTitle:"dfd",description:"dfdf",img:""},
-    {title:"dfd",subTitle:"dfd",description:"dfdfd",img:""},
-    {title:"dfd",subTitle:"dff",description:"dfdf",img:""},
-    {title:"dfdf",subTitle:"dfdf",description:"dfdff",img:""},
-]
+  {
+    title: "לינקדאין",
+    description:
+      "מדריך זה ינחה אותכם בבניית פרופיל שמתאים לתחילת תהליך בפיתוח קריירה בעולם הייטק",
+    img: "",
+    url: "https://e7.pngegg.com/pngimages/887/616/png-clipart-linkedin-icon-linkedin-text-rectangle-thumbnail.png",
+  },
+  {
+    title: "גיטהב",
+    description:
+      "מדריך שיכוון אתכם להשתמש נכון בגיטהב ובכל הפעולות שרצוי לדעת בעבודה עם גיטהב",
+    img: "https://e7.pngegg.com/pngimages/914/758/png-clipart-computer-icons-logo-github-github-logo-logo-computer-program-thumbnail.png",
+    url: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png",
+  },
+  {
+    title: "קורות חיים",
+    description: "מדריך לבניית קורות חיים מקצועיים שיתאימו לעולם ההייטק ",
+    img: "",
+    url: "https://resumeplus.co.il/how-to-write-cv/",
+  },
+  {
+    title: "ריאיון עבודה",
+    description: "מדריך שיכין אתכם לריאיון עבודה בצורה הכי טובה שיש",
+    img: "",
+    url: "https://www.taasuka.gov.il/he/Applicants/JobSearchStages/pages/jobinterview.aspx",
+  },
+];
 
 function Instructors() {
-    const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
-    const showModal = () => {
-      setIsModalVisible(true);
-    };
-  
-    const handleOk = () => {
-      setIsModalVisible(false);
-    };
-  
-    const handleCancel = () => {
-      setIsModalVisible(false);
-    };
-  
-    return (
-        <div>
-        <input placeholder="חפש מידע"/>
-       <Button type="primary" onClick={showModal}>
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    <div>
+      <Button type="primary" onClick={showModal}>
         הכנס מידע
       </Button>
-      <Modal title="באפשרותך לשתף מידע על מדריכים" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} okText="הוסף" cancelText="צא">
+      <Modal
+        title="באפשרותך לשתף מידע על מדריכים"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        okText="הוסף"
+        cancelText="צא"
+      >
         <div className="modal">
-        <input placeholder="נושא"/>
-        <input placeholder="קישור למדריך"/>
-        <textarea placeholder="מידע"/>
+          <input placeholder="נושא" />
+          <input placeholder="קישור למדריך" />
+          <textarea placeholder="מידע" />
         </div>
-        
-
       </Modal>
-            
-            
-        </div>
-    )
+      <div className="instructorsDiv">
+        {InstructorsArray.map((instruct) => {
+          return (
+            <div class="column">
+              <div class="post-module hover">
+                <div class="thumbnail">
+                  <img src={instruct.img} />
+                </div>
+                <div class="post-content">
+                  <h1 class="title">{instruct.title}</h1>
+                  <p class="description">{instruct.description}</p>
+                  <a href={instruct.url}>
+                    <button>קישור</button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Instructors;
