4a97278d0c891d8fc7b057878af6fd9144c899b8
Your Name <beniyaakov23@gmail.com>
2021-10-12 14:25:44 +0300

    beni\login

--- client/src/components/AppRouter/AppRouter.js
+++ client/src/components/AppRouter/AppRouter.js
@@ -3,6 +3,7 @@ import { Switch, Route ,Redirect } from 'react-router-dom';
 import {AuthContext} from "../Context/AuthContextProvider"
 import Profile from '../features/Profile/Profile';
 import Home from '../pages/Home/Home';
+import Login from "../features/Form/Login/Login"
 
 
 const AppRouter = () => {
@@ -11,12 +12,13 @@ const AppRouter = () => {
     if (userClearLocalStorage) {
       return <Redirect to='/logout' />;
     }
-    
+
     return (
         <div>
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route path="/icon" component={Profile} />
+                <Route path="/login" component={Login} />
 
             </Switch>
         </div>
