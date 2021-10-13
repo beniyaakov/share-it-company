3bc96c8b60d7a8215038ba3043b926800234c557
kineretasiyahan <kineret9688@gmail.com>
2021-10-13 11:46:16 +0300

    add changes

--- client/src/components/features/NavBar/NavBar.js
+++ client/src/components/features/NavBar/NavBar.js
@@ -14,10 +14,10 @@ const NavBar = () => {
         <div className="navBarMain">
                   {isLogin && <Profile />}
 {!isLogin&& 
-             <Link to='/Login'><Button type="primary" className="button" text="התחברות"></Button> </Link>
+             <Link to='/Login'><Button type="primary" className="button" text="התחברות" ></Button> </Link>
 }
              <img src="logo share it2.png" width="170" height="80px" style={{marginTop:10}} />
-             <Link to="../"><img src="https://www.ibh.co.il/wp-content/uploads/2018/06/%D7%98%D7%A7-%D7%A7%D7%A8%D7%99%D7%99%D7%A8%D7%94-2.png" width="100" /></Link>
+             <Link to="../"><img src="https://www.ibh.co.il/wp-content/uploads/2018/06/%D7%98%D7%A7-%D7%A7%D7%A8%D7%99%D7%99%D7%A8%D7%94-2.png" style={{width:100,marginRight:15}} /></Link>
         </div>
     )
 }
