e6abae79e37bcb2600fc25a93fe5566a8ea789fd
YehudaBayana <bayanayehuda@gmail.com>
2021-10-13 16:12:43 +0300

    remove forum from nav

--- client/src/components/features/NavBar/NavBar.js
+++ client/src/components/features/NavBar/NavBar.js
@@ -1,32 +1,35 @@
-import React,{useContext} from 'react'
-import { Link } from 'react-router-dom'
-import './NavBar.css'
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
 import 'antd/dist/antd.css';
-import Button from '../Button/Button'
-import Profile from '../Profile/Profile'
-import { AuthContext } from "../../Context/AuthContextProvider";
-import {Redirect} from "react-router-dom"
+import Button from '../Button/Button';
+import Profile from '../Profile/Profile';
+import { AuthContext } from '../../Context/AuthContextProvider';
+import { Redirect } from 'react-router-dom';
 import { Space } from 'antd';
 import { PoweroffOutlined } from '@ant-design/icons';
 
-
-
 const NavBar = () => {
-    const { isLogin } = useContext(AuthContext);
-    
-    return (
-       
-        <div className="navBarMain">
-                  {isLogin && <Profile />}
-{!isLogin&& 
-             <Link to='/Login'><Button type="primary" className="button" text="התחברות" ></Button> </Link>
-}
-{isLogin&&
-        <Link to=""><Button type="primary" className="button" text="פורום" className="porum"  ></Button></Link>
-        }
-<Link to="/userPage">    <img src="logo share it2.png" width="170" height="80px" style={{marginTop:10}} /></Link>
-         
-        </div>
-    )
-}
-export default NavBar
+  const { isLogin } = useContext(AuthContext);
+
+  return (
+    <div className='navBarMain'>
+      {isLogin && <Profile />}
+      {!isLogin && (
+        <Link to='/Login'>
+          <Button type='primary' className='button' text='התחברות'></Button>{' '}
+        </Link>
+      )}
+      <Link to='/userPage'>
+        {' '}
+        <img
+          src='logo share it2.png'
+          width='170'
+          height='80px'
+          style={{ marginTop: 10 }}
+        />
+      </Link>
+    </div>
+  );
+};
+export default NavBar;
