f6e14f29c6766cead9cfc30c8221d24f16f7789c
Your Name <beniyaakov23@gmail.com>
2021-10-13 11:03:30 +0300

    fix

--- client/src/components/features/Footer/Footer.js
+++ client/src/components/features/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './footer.css'
 import {FacebookOutlined,MailOutlined,InstagramOutlined } from '@ant-design/icons'
+import { Link } from 'react-router-dom'
 
 
 const Footer = () => {
@@ -9,6 +10,9 @@ const Footer = () => {
             <a href="https://www.facebook.com/tech.career"><FacebookOutlined style={{margin:"25px",color:"#87b4bb"}} ></FacebookOutlined></a>
             <a href="https://mail.google.com/"> <MailOutlined style={{margin:"25px",color:"#ede572"}}/></a>
              <a href="https://www.instagram.com/techcareer/?utm_medium=copy_link "> <InstagramOutlined style={{margin:"25px",color:"#f07575"}}/> </a>
+             <Link to="../"><img src="https://www.ibh.co.il/wp-content/uploads/2018/06/%D7%98%D7%A7-%D7%A7%D7%A8%D7%99%D7%99%D7%A8%D7%94-2.png" width="100" /></Link>
+
+             
         </div>
     )
 }
