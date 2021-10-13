42c2d1d40f4cfc4e7c3341a9fbad2e8f34e2c512
yosefsahalo <yosefsahalu@gmail.com>
2021-10-13 11:48:10 +0300

    favorites func

--- server/Route/userRoute.js
+++ server/Route/userRoute.js
@@ -1,8 +1,9 @@
 const rout = require('express').Router()
-const {UpdateUser,deleteUser,getAllUsers,getUserById } = require("../Controller/userController")
+const {UpdateUser,deleteUser,getAllUsers,getUserById, userFavorites } = require("../Controller/userController")
 rout.put('/updateUser',UpdateUser)
 rout.delete('/deleteUser',deleteUser)
 rout.get('/getAllUsers',getAllUsers)
 rout.get('/getUserById/:id',getUserById)
+rout.put('/userFavorites/:id',userFavorites)
 
 module.exports = rout
